03e359e2cc2abe4f8610c6b22203fed3f2b4394a
i-am-SR <sumitrawat101245@gmail.com>
2020-08-05 15:51:52 -0700

    Implementation of getCurrPrice function

--- script.js
+++ script.js
@@ -19,3 +19,36 @@ function addtoList() { // Function to update the display above the chart
   listItem.innerHTML = arr[arr.length - 1][0];
   listElement.appendChild(listItem);
 }
+
+function getCurrPrice(stock) {	//using jQuery to find the current price of the stock symbol
+  //console.log("getPrice called");
+  stock = stock.toUpperCase();
+    var my_url = 'https://finnhub.io/api/v1/quote?symbol=' + stock + '&token=brn8chfrh5rcnlf5iaj0';
+    $.ajax({
+      async: false,
+      url: my_url,
+      data: {
+        format: 'json'
+      },
+      error: function() {
+        // incase of error
+        console.log("Error in getting data from Finnhub.io");
+      },
+      success: function(data) {
+        if (data.hasOwnProperty('c')) { // If the Stock Symbol exists 
+          console.log(data.c);
+          if (!mySet.has(stock)){ //If the stock does not exists in the arr, add it
+          	stocktext.value = "";
+          	mySet.add(stock);
+    				arr.push(new Array(stock, data.c, "blue")); // add it to the array
+          	addtoList(); // display on the list
+          }
+          else{
+          	arr[i][1] = data.c; // else update its current cost
+          }  
+        }
+      },
+      type: 'GET'
+    });
+    //plot();
+}
